4acac36ab9afbb05f98444c65e8f9983a39c66c4
Sefa Akın <57005412+sefa1071@users.noreply.github.com>
2020-10-07 02:00:12 +0300

    lisr dosyası eklendi

---
+++ list.py
@@ -0,0 +1,29 @@
+
+urunler = ["laptop","mouse","keyboard"]
+print(urunler)
+# print(type(urunler))
+urunler.append("telefon")
+print(urunler)
+
+ogrenciler1 = ["zeyneb","eda","melike"]
+ogrenciler2 = ["şeyma","gamze","beyza"]
+
+ogrenciler1 = ogrenciler2
+ogrenciler2[0] ="melisa"
+
+print(ogrenciler1)
+print(ogrenciler2)
+
+sayi1 = 10
+sayi2 = 20
+sayi1 = sayi2
+sayi2 = 60
+print(sayi2)
+print(sayi1)
+
+# referans tip -> list, class, string
+# değer tip -> int
+
+isim="Zeynep"
+print(isim[0])
+
